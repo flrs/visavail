5a15bda0f3bfeb4085dce97cac748f1b7b0777d3
flrs <florian.roscheck@gmail.com>
2016-09-11 11:51:06 -0600

    Improve handling of empty data series
    It might happen that the user filters input
    data by date and, as a result, passes one or
    more empty data series to the function. This
    commit enables displaying empty data series.

--- visavail/js/visavail.js
+++ visavail/js/visavail.js
@@ -177,15 +177,17 @@ function visavailChart() {
       var endDate = 0;
 
       dataset.forEach(function (series, seriesI) {
-        if (seriesI === 0) {
-          startDate = series.disp_data[0][0];
-          endDate = series.disp_data[series.disp_data.length - 1][2];
-        } else {
-          if (series.disp_data[0][0] < startDate) {
+        if (series.disp_data.length>0) {
+          if (startDate === 0) {
             startDate = series.disp_data[0][0];
-          }
-          if (series.disp_data[series.disp_data.length - 1][2] > endDate) {
             endDate = series.disp_data[series.disp_data.length - 1][2];
+          } else {
+            if (series.disp_data[0][0] < startDate) {
+              startDate = series.disp_data[0][0];
+            }
+            if (series.disp_data[series.disp_data.length - 1][2] > endDate) {
+              endDate = series.disp_data[series.disp_data.length - 1][2];
+            }
           }
         }
       });
